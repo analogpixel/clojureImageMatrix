bc4b1b24fa3423e7a13f8104e06afb6658c49400
analogpixel <analog@analogpixel.org>
2015-01-09 20:08:33 -0700

    working image loading and saving

---
+++ project.clj
@@ -0,0 +1,11 @@
+
+(defproject image "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main image.core
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.mikera/core.matrix "0.32.1"]
+                 [net.mikera/vectorz-clj "0.28.0"]
+                 ])
